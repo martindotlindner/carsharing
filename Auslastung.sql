3e45eda6e1f5829a591024c37a99f51edc61a4b4
martindotlindner <martin.lindner@posteo.de>
2016-06-03 15:29:38 +0200

    Add first version

--- Auslastung.sql
+++
@@ -1,5 +0,0 @@
-COPY(
-SELECT provider, date_trunc('day',TIMESTAMPSTART) AS Date, COUNT(DISTINCT vehicleid) AS Vehicles, Count(*)
-FROM berlin.routes 
-GROUP BY provider, Date)
-TO '/tmp/Auslastung.csv' (format csv);

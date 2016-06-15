c9dacef9338d92f8d1f0f36449ac1fdd08c62e9c
martindotlindner <martin.lindner@posteo.de>
2016-06-16 00:16:42 +0200

    Update Import_CSV_SQL_Query.R

--- Preparation/R/Import_CSV_SQL_Query.R
+++ Preparation/R/Import_CSV_SQL_Query.R
@@ -1,14 +1,17 @@
-## Directory festlegen
+## Set Directory
 setwd("C:/Users/xxx/utf")
-## Liste mit csv-Dateien erstellen
+
+## Create list with csv-files
 filenames <- list.files(pattern = "\\.csv$") 
 sql_import <- matrix(ncol=1,nrow=length(filenames))
+
 for (i in 1:length(filenames)){
 sql_import[i] <- paste0("COPY world.routes (TIMESTAMPSTART, TIMESTAMPEND, PROVIDER, VEHICLEID, LICENCEPLATE, MODEL, INNERCLEANLINESS, OUTERCLEANLINESS, FUELTYPE, FUELSTATESTART, FUELSTATEEND, CHARGINGONSTART, CHARGINGONEND, STREETSTART, STREETEND, LATITUDESTART, LONGITUDESTART, LATITUDEEND, LONGITUDEEND)
 FROM 'C:/Users/xxx/utf/",filenames[i], "' NULL AS 'NA' DELIMITER ';' ;")
 }
+# Create sql-query
 write.table(sql_import, 
-            "sql_import.txt",
+            "sql_import.sql",
             quote=FALSE,
             row.names=FALSE,
             col.names = FALSE)

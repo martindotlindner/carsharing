ed6d4b7786bad2d3aad5dd4afb538e45be320e81
martindotlindner <martin.lindner@posteo.de>
2016-06-16 00:14:41 +0200

    Update and rename SQL_Import_CSV_Command.R to Import_CSV_SQL_Query.R

--- Preparation/R/Import_CSV_SQL_Query.R
+++ Preparation/R/Import_CSV_SQL_Query.R
@@ -1,11 +1,11 @@
 ## Directory festlegen
-setwd("C:/Users/Martin/Documents/Workaholic/TUD_Verkehr/Rohdaten/floating/utf")
+setwd("C:/Users/xxx/utf")
 ## Liste mit csv-Dateien erstellen
 filenames <- list.files(pattern = "\\.csv$") 
 sql_import <- matrix(ncol=1,nrow=length(filenames))
 for (i in 1:length(filenames)){
 sql_import[i] <- paste0("COPY world.routes (TIMESTAMPSTART, TIMESTAMPEND, PROVIDER, VEHICLEID, LICENCEPLATE, MODEL, INNERCLEANLINESS, OUTERCLEANLINESS, FUELTYPE, FUELSTATESTART, FUELSTATEEND, CHARGINGONSTART, CHARGINGONEND, STREETSTART, STREETEND, LATITUDESTART, LONGITUDESTART, LATITUDEEND, LONGITUDEEND)
-FROM 'C:/Users/Martin/Documents/Workaholic/TUD_Verkehr/Rohdaten/floating/utf/",filenames[i], "' NULL AS 'NA' DELIMITER ';' ;")
+FROM 'C:/Users/xxx/utf/",filenames[i], "' NULL AS 'NA' DELIMITER ';' ;")
 }
 write.table(sql_import, 
             "sql_import.txt",

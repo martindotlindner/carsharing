86bf5803e35831f8f5199df1c382775799b03271
martindotlindner <martin.lindner@posteo.de>
2016-06-16 13:38:36 +0200

    Add first version

---
+++ Preparation/PostgreSQL/Add_City.sql
@@ -0,0 +1,14 @@
+﻿-- Add column with city based on spatial query with operating area from Drivenow
+-- Import shapefiles with operating area of each city first
+ALTER TABLE world.routes ADD COLUMN city character varying;
+UPDATE world.routes SET city = CASE
+   WHEN ST_DWithin(world.routes.geom::geography, world.muenchen.geom::geography, 10000) THEN 'Muenchen'
+   WHEN ST_DWithin(world.routes.geom::geography, world.berlin.geom::geography, 5000) THEN 'Berlin'
+   WHEN ST_DWithin(world.routes.geom::geography, world.duesseldorf.geom::geography, 10000) THEN 'Duesseldorf'
+   WHEN ST_DWithin(world.routes.geom::geography, world.hamburg.geom::geography, 5000) THEN 'Hamburg'
+   WHEN ST_DWithin(world.routes.geom::geography, world.koeln.geom::geography, 10000) THEN 'Koeln'
+   ELSE 'unknown'
+END
+FROM world.muenchen WHERE ST_DWithin(world.routes.geom::geography, world.muenchen.geom::geography, 10000);
+;
+

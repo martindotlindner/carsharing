0c894bd5d1800ec79033c91ea51cd8668562bbb9
martindotlindner <martin.lindner@posteo.de>
2016-06-22 22:01:47 +0200

    Update

--- Analysis/PostgreSQL/Hexagon_Matrix_Start_End.sql
+++ Analysis/PostgreSQL/Hexagon_Matrix_Start_End.sql
@@ -1,15 +1,30 @@
---Modify table names and schemas if necessary!
+﻿ALTER TABLE berlin.routes DROP COLUMN if exists geom_end;
+SELECT  AddGeometryColumn(
+	'berlin',
+	'routes',
+	'geom_end',
+	25833,
+	'POINT'
+	,2);
+
+UPDATE berlin.routes SET geom_end = ST_Transform(ST_SetSRID(ST_Point(longitudeend, latitudeend), 4326), 25833);
+
+SELECT * FROM berlin.routes ORDER BY timestampstart desc LIMIT 100;
+
 ALTER TABLE berlin.routes ADD COLUMN start_hex integer;
 ALTER TABLE berlin.routes ADD COLUMN end_hex integer;
 
 
-UPDATE berlin.routes SET start_hex = gid FROM berlin.hexagon_empty WHERE ST_Within(berlin.routes.geom_start, berlin.hexagon_empty.geom);
-UPDATE berlin.routes SET end_hex = gid FROM berlin.hexagon_empty WHERE ST_Within(berlin.routes.geom_end, berlin.hexagon_empty.geom);
+UPDATE berlin.routes SET start_hex = gid FROM berlin.hexagon_1km WHERE ST_Within(berlin.routes.geom_start, berlin.hexagon_1km.geom);
+UPDATE berlin.routes SET end_hex = gid FROM berlin.hexagon_1km WHERE ST_Within(berlin.routes.geom_end, berlin.hexagon_1km.geom);
 
 
 COPY(
-SELECT start_hex,end_hex, count(*) FROM berlin.routes GROUP BY start_hex, end_hex ORDER BY start_hex, end_hex)
+SELECT start_hex,end_hex, count(*) FROM berlin.routes WHERE TIMESTAMPSTART < '2015-06-01'::timestamp AND TIMESTAMPSTART > '2015-03-01'::timestamp GROUP BY start_hex, end_hex ORDER BY start_hex, end_hex)
 --Windows
---TO 'C:/Program Files/PostgreSQL/9.5/data/Hexagon_Matrix.csv' DELIMITER ';' CSV HEADER;
---Linux
-TO '/tmp/Hexagon_Matrix.csv' DELIMITER ';' CSV HEADER;
+TO 'C:/Program Files/PostgreSQL/9.5/data/Hexagon_Matrix_before.csv' DELIMITER ';' CSV HEADER;
+
+COPY(
+SELECT start_hex,end_hex, count(*) FROM berlin.routes WHERE TIMESTAMPSTART < '2015-09-01'::timestamp AND TIMESTAMPSTART > '2015-06-01'::timestamp GROUP BY start_hex, end_hex ORDER BY start_hex, end_hex)
+--Windows
+TO 'C:/Program Files/PostgreSQL/9.5/data/Hexagon_Matrix_after.csv' DELIMITER ';' CSV HEADER;
---
+++ Preparation/PostgreSQL/Remove_Errors_Step1.sql
@@ -0,0 +1,29 @@
+﻿-- For Berlin
+/*
+-- Drop all trips from Multicity
+DELETE FROM berlin.routes
+WHERE provider = 'MULTICITY';
+
+-- Drop 'Umrüsterfahrten'
+
+DELETE FROM berlin.routes
+WHERE streetstart = 'Umrüster DE' OR streetend = 'Umrüster DE' ;
+
+DELETE FROM berlin.routes
+WHERE latitudestart < 1 OR longitudestart < 1 OR latitudeend < 1 OR latitudeend < 1;
+
+*/
+
+-- For Germany
+
+-- Drop all trips from Multicity
+DELETE FROM germany.routes
+WHERE provider = 'MULTICITY';
+
+-- Drop 'Umrüsterfahrten'
+
+DELETE FROM germany.routes
+WHERE streetstart = 'Umrüster DE' OR streetend = 'Umrüster DE' ;
+
+DELETE FROM germany.routes
+WHERE latitudestart < 1 OR longitudestart < 1 OR latitudeend < 1 OR latitudeend < 1;

d7e81683f2d0f7e75fd3fa4562abce3820d7ab2f
martindotlindner <martin.lindner@posteo.de>
2016-06-24 01:06:21 +0200

    Add first version

--- Analysis/PostgreSQL/Nearest_Neighbour.sql
+++ Analysis/PostgreSQL/Nearest_Neighbour.sql
@@ -1,17 +1,23 @@
-﻿SELECT * FROM berlin.punkte_100m_oa WHERE GID = 1;
+﻿ALTER TABLE berlin.punkte_100m_oa DROP COLUMN if exists id, DROP COLUMN polygon;
+-- Remove duplicates but keep the first row of each group
 
-SELECT ST_AsText(geom_start) AS "geomstart_txt", ST_AsText(ST_ClosestPoint(berlin.routes.geom_start, berlin.punkte_100m_oa.geom)) FROM berlin.routes, berlin.punkte_100m_oa WHERE GID = 1 LIMIT 10;
+-- Attention! Very time consuming (try with a small selection first)
+SELECT * INTO berlin.routes_sel FROM berlin.routes LIMIT 10000;
 
-COPY(
-SELECT berlin.routes.*, ST_Distance(geom_start, berlin.punkte_100m_oa.geom)/1000 AS distance_km
-FROM berlin.routes, berlin.punkte_100m_oa 
-WHERE ST_DWithin(geom_start, berlin.punkte_100m_oa.geom, 100000) AND berlin.punkte_100m_oa.gid = 1
-ORDER BY ST_Distance(geom_start, berlin.punkte_100m_oa.geom)
-LIMIT 10)
-TO 'C:/Program Files/PostgreSQL/9.5/data/Nearest_Point.csv' (format csv);
+-- General way
+SELECT 
+   DISTINCT ON (berlin.punkte_100m_oa.gid) berlin.punkte_100m_oa.gid, berlin.routes_sel.id, ST_Distance(berlin.routes_sel.geom_start, berlin.punkte_100m_oa.geom)  as dist
+FROM berlin.punkte_100m_oa, berlin.routes_sel 
+ORDER BY berlin.punkte_100m_oa.gid, berlin.routes_sel.id, ST_Distance(berlin.routes_sel.geom_start, berlin.punkte_100m_oa.geom) LIMIT 100;
 
-SELECT berlin.punkte_100m_oa.gid,berlin.routes.id, avg(ST_Distance(geom_start, berlin.punkte_100m_oa.geom)/1000) AS distance_km
-FROM berlin.routes, berlin.punkte_100m_oa 
-WHERE ST_DWithin(geom_start, berlin.punkte_100m_oa.geom, 100000) AND berlin.punkte_100m_oa.gid = 1
-ORDER BY ST_Distance(geom_start, berlin.punkte_100m_oa.geom)
-LIMIT 3;
+-- If you know a max distance between both points, you can accelerate calculation
+
+SELECT 
+   DISTINCT ON (berlin.punkte_100m_oa.gid) berlin.punkte_100m_oa.gid, berlin.routes_sel.id, ST_Distance(berlin.routes_sel.geom_start, berlin.punkte_100m_oa.geom)  as dist
+FROM berlin.punkte_100m_oa, berlin.routes_sel 
+WHERE ST_DWithin(berlin.routes_sel.geom_start, berlin.punkte_100m_oa.geom,5000) 
+ORDER BY berlin.punkte_100m_oa.gid, berlin.routes_sel.id, ST_Distance(berlin.routes_sel.geom_start, berlin.punkte_100m_oa.geom) LIMIT 100;
+
+
+
+

af05829a645190910fe3b3747db54ce580129b2b
martindotlindner <martin.lindner@posteo.de>
2016-06-27 01:26:16 +0200

    Update

--- Analysis/R/Hexagon_Matrix_Start_End.R
+++ Analysis/R/Hexagon_Matrix_Start_End.R
@@ -1,23 +1,31 @@
 library(reshape2)
 
-setwd("C:/Program Files/PostgreSQL/9.5/data/")
+# setwd("C:/Program Files/PostgreSQL/9.5/data/")
+setwd("C:/Users/Martin/Documents/Workaholic/TUD_Verkehr/R/Data/")
 
 # Read table from sql query "Hexagon_Matrix_Start_End.sql"
-hex_matrix <- read.table("Hexagon_Matrix.csv",header = FALSE, sep = ",")
-colnames(hex_matrix) <- c("start_hex","end_hex","count")
+hex_matrix_before <- read.table("Hexagon_Matrix_before.csv",header = TRUE, sep = ";")
+hex_matrix_after <- read.table("Hexagon_Matrix_after.csv",header = TRUE, sep = ";")
+
 
 #Create column with percentage
-hex_matrix$Anteil <- (hex_matrix$count/sum(hex_matrix$count))*100
+hex_matrix_before$Anteil <- (hex_matrix_before$count/sum(hex_matrix_before$count))*100
+hex_matrix_after$Anteil <- (hex_matrix_after$count/sum(hex_matrix_after$count))*100
 
 #Remove rows with NA
-hex_matrix <- na.omit(hex_matrix)
+hex_matrix_before <- na.omit(hex_matrix_before)
+hex_matrix_after <- na.omit(hex_matrix_after)
 
 #Create matrix from data frame
-hex_matrix_total <- acast(hex_matrix, start_hex~end_hex, value.var = "count")
-hex_matrix_anteil <- acast(hex_matrix, start_hex~end_hex, value.var = "Anteil")
+hex_matrix_before_total <- acast(hex_matrix_before, start_hex~end_hex, value.var = "count")
+hex_matrix_after_total <- acast(hex_matrix_after, start_hex~end_hex, value.var = "count")
+#hex_matrix_anteil <- acast(hex_matrix, start_hex~end_hex, value.var = "Anteil")
+#hex_matrix_anteil <- acast(hex_matrix, start_hex~end_hex, value.var = "Anteil")
 
 ## Set directory before writing csv
 #setwd("")
-write.csv(hex_matrix_total, file = "hex_matrix_total.csv")
-write.csv(hex_matrix_anteil, file = "hex_matrix_anteil.csv")
+write.csv(hex_matrix_before_total, file = "hex_matrix_before_total.csv")
+write.csv(hex_matrix_after_total, file = "hex_matrix_after_total.csv")
+
+#write.csv(hex_matrix_anteil, file = "hex_matrix_anteil.csv")
 
--- Analysis/R/Sales_per_Vehicles.R
+++ Analysis/R/Sales_per_Vehicles.R
@@ -1,25 +1,34 @@
 library(ggplot2)
 library(plyr)
-library(zoo)
-#setwd("C:/Program Files/PostgreSQL/9.5/data/")
-setwd("C:/Users/Martin/Documents/Workaholic/TUD_Verkehr/R/Data/")
+
+setwd("C:/Program Files/PostgreSQL/9.5/data/")
+# setwd("C:/Users/Martin/Documents/Workaholic/TUD_Verkehr/R/Data/")
 
 
-sales_vehicle <- read.table("Sales_Vehicles.csv",sep = ";",header = TRUE)
+sales_vehicle <- read.table("Sales_Vehicles_City.csv",sep = ";",header = TRUE)
 sales_vehicle$salespervehicle <- sales_vehicle$sum/sales_vehicle$vehicles
 sales_vehicle$date <- as.POSIXct(strptime(sales_vehicle$date, format = "%Y-%m-%d %H:%M:%S"))
 
 sales_vehicle$day <- weekdays(as.Date(sales_vehicle$date))
 sales_vehicle$shortdate <- strftime(sales_vehicle$date, format="%Y-%m")
 
-ggplot(sales_vehicle,aes(date,salespervehicle, colour = provider)) +
-  geom_line()
 
-sales_sum <- ddply(sales_vehicle, .(shortdate,provider), summarize, SalesperDay = mean(salespervehicle,na.rm = TRUE))
+#sales_mean <- ddply(sales_vehicle, .(shortdate,provider,city), summarize, SalesperDay = mean(salespervehicle,na.rm = TRUE))
+sales_mean <- ddply(sales_vehicle, .(provider,city), summarize, SalesperDay = mean(salespervehicle,na.rm = TRUE))
+vehicles_mean <- ddply(sales_vehicle, .(provider,city), summarize, Vehicles = mean(vehicles,na.rm = TRUE))
+
+sales_mean <- sales_mean[-which(sales_mean$city == ""), ]
+newrow <- c("DRIVENOW", "Frankfurt", NA)
+sales_mean <- rbind(sales_mean, newrow)
+sales_mean$SalesperDay <- as.numeric(sales_mean$SalesperDay)
+
+ggplot(data=sales_mean, aes(x=city, y=SalesperDay)) +
+  geom_bar(aes(fill = provider),colour = "black", position = "dodge", stat="identity")
 
 
-sales_sum$shortdate <- as.yearmon(sales_sum$shortdate)
-sales_sum$shortdate <- as.POSIXct(sales_sum$shortdate)
-ggplot(sales_sum, aes(shortdate,SalesperDay,colour = provider)) +
+
+sales_mean$shortdate <- as.yearmon(sales_mean$shortdate)
+sales_mean$shortdate <- as.POSIXct(sales_mean$shortdate)
+ggplot(sales_mean, aes(shortdate,SalesperDay,colour = provider)) +
   geom_line()
 

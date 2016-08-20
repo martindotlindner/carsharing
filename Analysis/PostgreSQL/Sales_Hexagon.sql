0db1f7c7429dad6e7b6ef9384ce9678249a9d2b7
martindotlindner <martin.lindner@posteo.de>
2016-08-20 14:44:00 +0200

    Update Sales_Hexagon.sql

--- Analysis/PostgreSQL/Sales_Hexagon.sql
+++ Analysis/PostgreSQL/Sales_Hexagon.sql
@@ -3,4 +3,4 @@ SELECT berlin.hexagon_1km.gid, ST_AsText(berlin.hexagon_1km.geom), SUM(berlin.ro
  FROM berlin.hexagon_1km, berlin.routes
  WHERE ST_Within(berlin.routes.geom_start,berlin.hexagon_1km.geom)
  GROUP BY berlin.hexagon_1km.gid)
- TO '/tmp/sales_starts_hexagon.csv' (format csv);
+ TO '/tmp/sales_starts_hexagon.csv' DELIMITER ';' CSV HEADER;

b89e01e3aac846ed70c46c5ee75dcda3689e6108
martindotlindner <martin.lindner@posteo.de>
2016-08-20 11:49:55 +0200

    Add new version

--- Analysis/PostgreSQL/Lenght_Lines_Hexagon.sql
+++ Analysis/PostgreSQL/Lenght_Lines_Hexagon.sql
@@ -1,4 +1,4 @@
-﻿--Calculate lenght of lines within each hexagon
+﻿--Calculate lenght of lines ('berlin.cycleway_25833') within each hexagon
 SELECT berlin.hexagon_empty.gid, ST_AsText(berlin.hexagon_empty.geom), ST_LENGTH(ST_Intersection(berlin.hexagon_empty.geom, berlin.cycleway_25833.geom))
 FROM berlin.hexagon_empty, berlin.cycleway_25833
 WHERE ST_Intersects(berlin.hexagon_empty.geom, berlin.cycleway_25833.geom);
--- Data/R/operatingarea_car2go.R
+++ Data/R/operatingarea_car2go.R
@@ -1,11 +1,11 @@
 # Replacement of umlauts and spaces have to be added (Failure of downloading e.g. 'München" or 'New York City'
 library(curl,RCurl,httr,jsonlite)
-library(jsonlite)
 
 cities <- fromJSON("https://www.car2go.com/api/v2.1/locations?oauth_consumer_key=consumer_key&format=json")
 citynames <- tolower(cities$location$locationName)
 
-city = c("stuttgart","berlin","hamburg","frankfurt","rheinland","muenchen")
+# Download kml-files for following cities:
+city = c("stuttgart","berlin","hamburg","frankfurt","rheinland","muenchen","seattle","vancouver","calgary")
 for (i in 1:length(citynames)){
 url <- paste0("http://www.car2go.com/api/v2.1/operationareas?loc=",citynames[i],"&oauth_consumer_key=car2gowebsite")
 filename <- paste0(citynames[i],".kml")

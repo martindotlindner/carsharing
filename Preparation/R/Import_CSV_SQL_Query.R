753f3afd0427006bbf2499944d428e1585e4ee9a
martindotlindner <martin.lindner@posteo.de>
2016-09-13 21:00:33 +0200

    Update Import_CSV_SQL_Query.R

--- Preparation/R/Import_CSV_SQL_Query.R
+++ Preparation/R/Import_CSV_SQL_Query.R
@@ -1,13 +1,17 @@
-## Set Directory
+### Create sql script to import multiple CVS files into a PostgreSQL database
+
+# Create directory with all CSV files to import
+# Set directory of CSV files
 setwd("C:/Users/xxx/utf")
 
-## Create list with csv-files
+# Create list with  CSV files
 filenames <- list.files(pattern = "\\.csv$") 
 sql_import <- matrix(ncol=1,nrow=length(filenames))
 
+
 for (i in 1:length(filenames)){
-sql_import[i] <- paste0("COPY world.routes (TIMESTAMPSTART, TIMESTAMPEND, PROVIDER, VEHICLEID, LICENCEPLATE, MODEL, INNERCLEANLINESS, OUTERCLEANLINESS, FUELTYPE, FUELSTATESTART, FUELSTATEEND, CHARGINGONSTART, CHARGINGONEND, STREETSTART, STREETEND, LATITUDESTART, LONGITUDESTART, LATITUDEEND, LONGITUDEEND)
-FROM 'C:/Users/xxx/utf/",filenames[i], "' NULL AS 'NA' DELIMITER ';' ;")
+  sql_import[i] <- paste0("COPY world.routes (TIMESTAMPSTART, TIMESTAMPEND, PROVIDER, VEHICLEID, LICENCEPLATE, MODEL, INNERCLEANLINESS, OUTERCLEANLINESS, FUELTYPE, FUELSTATESTART, FUELSTATEEND, CHARGINGONSTART, CHARGINGONEND, STREETSTART, STREETEND, LATITUDESTART, LONGITUDESTART, LATITUDEEND, LONGITUDEEND)
+                          FROM '",getwd(),"/",filenames[i], "' NULL AS 'NA' DELIMITER ';' ;")
 }
 # Create sql-query
 write.table(sql_import, 
@@ -15,3 +19,4 @@ write.table(sql_import,
             quote=FALSE,
             row.names=FALSE,
             col.names = FALSE)
+

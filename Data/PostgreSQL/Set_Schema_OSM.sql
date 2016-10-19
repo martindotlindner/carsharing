9f5f76f3050c62c7a7eff7f34de23c28281643b6
martindotlindner <martin.lindner@posteo.de>
2016-10-19 20:32:16 +0200

    Update Set_Schema_OSM.sql

--- Data/PostgreSQL/Set_Schema_OSM.sql
+++ Data/PostgreSQL/Set_Schema_OSM.sql
@@ -1,13 +1,13 @@
 ﻿CREATE SCHEMA if not exists osm;
 
 -- Set schema for berlin_osm
-ALTER TABLE berlin_osm_line SET SCHEMA osm;
-ALTER TABLE berlin_osm_nodes SET SCHEMA osm;
-ALTER TABLE berlin_osm_point SET SCHEMA osm;
-ALTER TABLE berlin_osm_polygon SET SCHEMA osm;
-ALTER TABLE berlin_osm_rels SET SCHEMA osm;
-ALTER TABLE berlin_osm_roads SET SCHEMA osm;
-ALTER TABLE berlin_osm_ways SET SCHEMA osm;
+--ALTER TABLE berlin_osm_line SET SCHEMA osm;
+--ALTER TABLE berlin_osm_nodes SET SCHEMA osm;
+--ALTER TABLE berlin_osm_point SET SCHEMA osm;
+--ALTER TABLE berlin_osm_polygon SET SCHEMA osm;
+--ALTER TABLE berlin_osm_rels SET SCHEMA osm;
+--ALTER TABLE berlin_osm_roads SET SCHEMA osm;
+--ALTER TABLE berlin_osm_ways SET SCHEMA osm;
 
 -- Set schema for germany_osm
 ALTER TABLE germany_osm_line SET SCHEMA osm;

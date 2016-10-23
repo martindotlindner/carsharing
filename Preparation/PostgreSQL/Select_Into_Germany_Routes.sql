7502911933bf10afee2bd7a0468f63b45ba057b6
Martin Lindner <martin.lindner@posteo.de>
2016-10-23 20:18:45 +0200

    Update Select_Into_Germany_Routes.sql

--- Preparation/PostgreSQL/Select_Into_Germany_Routes.sql
+++ Preparation/PostgreSQL/Select_Into_Germany_Routes.sql
@@ -1,10 +1,10 @@
 ﻿-- Select all columns to new table
 CREATE SCHEMA if not exists germany ;
 DROP TABLE if exists germany.routes ;
-SELECT world.routes.* INTO germany.routes FROM world.routes WHERE ST_DWithin(world.routes.geom::geography, ST_MakePolygon(ST_GeomFromText('LINESTRING(6 55, 15 55, 15 47.5, 6 47.5,6 55)')) ,1000);
+SELECT world.routes.* INTO germany.routes FROM world.routes WHERE ST_DWithin(world.routes.geom_start::geography, ST_MakePolygon(ST_GeomFromText('LINESTRING(6 55, 15 55, 15 47.5, 6 47.5,6 55)')) ,1000);
 
 -- Create two tables, on with important columns (germany.routes) and one with rarely used information (germany.routes_meta)
- 
+/* 
 DROP TABLE if exists germany.routes_info ;
 SELECT ID, TIMESTAMPSTART, TIMESTAMPEND, PROVIDER, VEHICLEID, LATITUDESTART, LONGITUDESTART, LATITUDEEND, LONGITUDEEND
 INTO germany.routes_info FROM world.routes WHERE ST_DWithin(world.routes.geom::geography, ST_MakePolygon(ST_GeomFromText('LINESTRING(6 55, 15 55, 15 47.5, 6 47.5,6 55)')) ,1000);
@@ -12,3 +12,4 @@ INTO germany.routes_info FROM world.routes WHERE ST_DWithin(world.routes.geom::g
 DROP TABLE if exists germany.routes_meta ;
 SELECT ID, LICENCEPLATE, MODEL, INNERCLEANLINESS, OUTERCLEANLINESS, FUELTYPE, FUELSTATESTART, FUELSTATEEND, CHARGINGONSTART, CHARGINGONEND, STREETSTART, STREETEND
 INTO germany.routes_meta FROM world.routes WHERE ST_DWithin(world.routes.geom::geography, ST_MakePolygon(ST_GeomFromText('LINESTRING(6 55, 15 55, 15 47.5, 6 47.5,6 55)')) ,1000);
+*/

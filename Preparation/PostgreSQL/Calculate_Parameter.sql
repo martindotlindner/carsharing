fe8871ea61187b8647486cd9abe6b344a8aaf2fa
Martin Lindner <martin.lindner@posteo.de>
2016-10-24 20:13:10 +0200

    Update Calculate_Parameter.sql

--- Preparation/PostgreSQL/Calculate_Parameter.sql
+++ Preparation/PostgreSQL/Calculate_Parameter.sql
@@ -18,10 +18,3 @@ CREATE INDEX idx_germany_routes_duration_min ON germany.routes(duration_min);
 ALTER TABLE germany.routes ADD COLUMN distance_m double precision;
 UPDATE germany.routes SET distance_m = ROUND(ST_Length(geom)::numeric,2) ;
 
---Mean speed in km/h
-ALTER TABLE germany.routes ADD COLUMN mean_speed double precision;
-UPDATE germany.routes SET mean_speed = (distance_m/1000)/(duration_min/60) ;  
-
-DROP INDEX if exists idx_germany_routes_speed;
-CREATE INDEX idx_germany_routes_speed ON germany.routes(mean_speed);
-

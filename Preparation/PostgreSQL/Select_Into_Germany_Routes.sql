ba041d3fa73f27d3584ce824f9c8ba91dc462aee
Martin Lindner <martin.lindner@posteo.de>
2016-10-25 10:52:27 +0200

    Update Select_Into_Germany_Routes.sql

--- Preparation/PostgreSQL/Select_Into_Germany_Routes.sql
+++ Preparation/PostgreSQL/Select_Into_Germany_Routes.sql
@@ -1,4 +1,4 @@
-﻿-- Select all columns to new table
+-- Select all columns to new table
 CREATE SCHEMA if not exists germany ;
 DROP TABLE if exists germany.routes ;
 SELECT world.routes.* INTO germany.routes FROM world.routes WHERE ST_DWithin(world.routes.geom_start::geography, ST_MakePolygon(ST_GeomFromText('LINESTRING(6 55, 15 55, 15 47.5, 6 47.5,6 55)')) ,1000);
